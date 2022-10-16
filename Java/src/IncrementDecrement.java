5749df93a90746e0adc810ae87589d25ca20f8aa
varshini <Varshinivarsha54354@gmail.com>
2022-10-16 13:54:52 +0530

    first commit

---
+++ Java/src/IncrementDecrement.java
@@ -0,0 +1,15 @@
+
+public class IncrementDecrement {
+
+	public static void main(String[] args) {
+		int a=10;
+		int b=4;
+		int c=9;
+		a=a++ + b--;//10+ 4=14
+		System.out.println(a);//14
+		System.out.println(b);//3
+		System.out.println(c);//9
+
+	}
+
+}
