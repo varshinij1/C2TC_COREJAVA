07c2666a7c823183091a9ae8acb749eae5c01f26
varshini <Varshinivarsha54354@gmail.com>
2022-10-16 13:57:48 +0530

    first commit

---
+++ Java/src/IfExample.java
@@ -0,0 +1,17 @@
+
+public class IfExample {
+
+	public static void main(String[] args) {
+		int a=3;
+		if(a==3)
+		{
+			System.out.println("Valid");
+		}
+		else
+		{
+			System.out.println("Invalid");
+		}
+
+	}
+
+}
