dff852e9a2f6809a941f0478ac44c5499833fabf
varshini <Varshinivarsha54354@gmail.com>
2022-10-16 13:56:16 +0530

    first commit

---
+++ Java/src/AssignmentOperator.java
@@ -0,0 +1,17 @@
+
+public class AssignmentOperator {
+
+	public static void main(String[] args) {
+		 int a=1;
+	     int b=2;
+	     int c=3;
+	     a+=5;
+	     b*=4;
+	     c+=a*b;
+	     c%=6;// % means modulus it will give remainder as the answer
+	     System.out.println(a);
+	     System.out.println(b);
+	     System.out.println(c);
+	}
+
+}
