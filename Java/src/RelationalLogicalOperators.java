c241c0a4111556563a9c184a2106c09e28177994
varshini <Varshinivarsha54354@gmail.com>
2022-10-16 13:38:40 +0530

    first commit

---
+++ Java/src/RelationalLogicalOperators.java
@@ -0,0 +1,23 @@
+import java.util.Random;//we used random keyword hence we are importing
+public class RelationalLogicalOperators {
+
+	public static void main(String[] args) {
+		Random rand=new Random();
+	     //Random() is a object in order to create a object we need to use new keyword 
+	      //and we are assigning the object to some object reference variable 
+	       int i=rand.nextInt(10);
+	       //we are taking the integer value hence nextInt from random hence .rand and we are assign 
+	       //it to int i variable and 10 is the range within that range the values will be generatedz
+	       
+	       int j=rand.nextInt(10);
+	       System.out.println("i="+i);
+	       System.out.println("j="+j);
+		}
+
+	
+
+
+
+	}
+
+
