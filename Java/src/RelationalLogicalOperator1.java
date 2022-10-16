9ea97558457d601f2d271c30eeb5c62a92cf921f
varshini <Varshinivarsha54354@gmail.com>
2022-10-16 13:41:50 +0530

    first commit

---
+++ Java/src/RelationalLogicalOperator1.java
@@ -0,0 +1,22 @@
+import java.util.Random;
+public class RelationalLogicalOperator1 {
+
+	public static void main(String[] args) {
+		 Random rand=new Random();
+    	 int i=rand.nextInt(10);
+    	 int j=rand.nextInt(10);
+    	 System.out.println("i="+i);
+    	 System.out.println("j="+j);
+    	 System.out.println(j>i);
+    	 System.out.println(j<i);
+    	 System.out.println(i>=j);
+    	 System.out.println(i!=j);
+	
+}
+
+
+
+
+	}
+
+
