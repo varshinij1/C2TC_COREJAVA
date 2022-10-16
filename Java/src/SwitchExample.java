6ce671005951e84de34146d7db530affce6e4328
varshini <Varshinivarsha54354@gmail.com>
2022-10-16 14:03:41 +0530

    first commit

---
+++ Java/src/SwitchExample.java
@@ -0,0 +1,41 @@
+
+public class SwitchExample {
+
+	public static void main(String[] args) {
+		int i=0;
+        switch(i) {
+       // case 0:
+      	 // System.out.println("zero");
+        //case 1:
+      	 // System.out.println("one");
+        //case 2:
+      	  //System.out.println("Two");
+     
+        
+        
+        
+        case 0:
+        	System.out.println("zero");
+        	break;
+        case 1:
+        	System.out.println("one");
+        	break;
+        case 2:
+        	System.out.println("two");
+        	break;
+        case 3:
+        	System.out.println("Three");
+        	break;
+        	default:System.out.println("Invalid");
+        
+        
+        
+        
+        
+        
+        }   
+     
+
+	}
+
+}
