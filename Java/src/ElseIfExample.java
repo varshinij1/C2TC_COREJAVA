422f8429441ebf0e468e3855fcec3b7b8a6e3f9a
varshini <Varshinivarsha54354@gmail.com>
2022-10-16 13:59:21 +0530

    first commit

---
+++ Java/src/ElseIfExample.java
@@ -0,0 +1,24 @@
+
+public class ElseIfExample {
+
+	public static void main(String[] args) {
+		int a=4;
+		if(a==1)
+		{
+			System.out.println("one");
+		}
+		else if(a==2)
+		{
+			System.out.println("Two");
+		}
+		else if(a==3)
+		{
+			System.out.println("Three");
+		}
+		else 
+		{
+			System.out.println("Ivalid");
+		}
+	}
+
+}
