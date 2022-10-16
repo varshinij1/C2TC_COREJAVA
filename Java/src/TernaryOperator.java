0f9b2e95d28569fdfaa27e6215d343769ce1dc69
varshini <Varshinivarsha54354@gmail.com>
2022-10-16 13:49:54 +0530

    first commit

---
+++ Java/src/TernaryOperator.java
@@ -0,0 +1,17 @@
+
+public class TernaryOperator {
+
+	public static void main(String[] args) {
+		// int v=1;
+		//System.out.println(v==1 ? "Hello":"World");//if v==1 then it prints hello if it is false it prints world
+
+		
+		
+		
+		int v=1;
+		System.out.println(v==1 ? "Hello":"World");
+		v++;
+		System.out.println(v==1 ? "Hello":"world");
+	}
+
+}
